34e68c06897947bfed4cc033ce5d88dc5e13d8b6
Piyush Mehta <me@piyushmehta.com>
2018-07-13 18:14:14 +0530

    Page index added
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+ import React, { Component } from 'react';
+    import Layout from '../components/Layout';
+    
+    class IndexPage extends Component {
+    	render() {
+    		<h1>Ye to bas shuruwaat hai<h1>
+    	}
+export default () => (
+	<IndexPage />
+);
